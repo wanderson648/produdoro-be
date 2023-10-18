43153023a07293dc12baadc0afdadff824ae9857
wanderson648 <testepedroca@gmail.com>
2023-10-17 21:29:57 -0300

    mapeia projeto

---
+++ src/main/java/dev/wakandaacademy/produdoro/projeto/domain/Projeto.java
@@ -0,0 +1,24 @@
+package dev.wakandaacademy.produdoro.projeto.domain;
+
+import lombok.*;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor(access = AccessLevel.PRIVATE)
+@AllArgsConstructor(access = AccessLevel.PRIVATE)
+@Document(collation = "Projeto")
+@Data
+public class Projeto {
+    @Id
+    private UUID idProjeto;
+    private String nome;
+    private String descricao;
+    @Indexed
+    private UUID idUsuario;
+    @Indexed
+    private UUID idArea;
+}
