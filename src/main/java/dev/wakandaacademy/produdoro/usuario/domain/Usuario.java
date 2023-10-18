9b4f6a98de5304c98b46824544f98836128bceb6
wanderson648 <testepedroca@gmail.com>
2023-10-17 21:15:32 -0300

    implementa usuario domain

---
+++ src/main/java/dev/wakandaacademy/produdoro/usuario/domain/Usuario.java
@@ -0,0 +1,30 @@
+package dev.wakandaacademy.produdoro.usuario.domain;
+
+import jakarta.validation.constraints.Email;
+import lombok.AllArgsConstructor;
+import lombok.Builder;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.springframework.data.annotation.Id;
+import org.springframework.data.mongodb.core.index.Indexed;
+import org.springframework.data.mongodb.core.mapping.Document;
+
+import java.util.UUID;
+
+@Builder
+@NoArgsConstructor
+@AllArgsConstructor
+@Document(collation = "Usuario")
+@Data
+public class Usuario {
+    @Id
+    private UUID idUsuario;
+    @Email
+    @Indexed
+    private String email;
+    private ConfiguracaoUsuario configuracaoUsuario;
+    @Builder.Default
+    private StatusUsuario statusUsuario = StatusUsuario.FOCO;
+    @Builder.Default
+    private Integer quantidadePomodorosPausaCurta = 0;
+}
